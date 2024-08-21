3c6a8776bec5b4ae4fa48872d813bc9e0efbd732
twani <jbestius@gmail.com>
2024-08-21 15:30:57 +0200

    add tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 
 import './App.css'
+import AnimatedText from './components/animatedText'
 
 function App() {
 
   return (
-    <div>
-      Hello
+    <div className="flex flex-col items-center justify-center h-screen">
+      <AnimatedText />
     </div>
 
   )
---
+++ src/components/animatedText.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AnimatedText = () => {
+  return (
+    <div className="py-8">
+        <h1>Hello Uganda</h1>
+    </div>
+  )
+}
+
+export default AnimatedText
