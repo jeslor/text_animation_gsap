25c0ec86096c6dc05d8034cb6d5a7b9c6da48868
twani <jbestius@gmail.com>
2024-08-21 16:29:37 +0200

    add the video

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 
-import './App.css'
+
 import AnimatedText from './components/animatedText'
+import styles from './App.module.css';
 
 function App() {
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <AnimatedText />
+    <div className={`flex flex-col items-center justify-center h-screen w-screen bg-black relative ${styles.main}`}>
+      <div className={`${styles.myGradient} absolute z-[1] h-full w-full`}></div>
+      <video autoPlay muted loop className='h-full w-full object-cover'>
+        <source src="/videos/myVideo.webm" type="video/webm"/>
+        Your browser does not support the video tag.
+      </video>
+     <div className="absolute z-[2]">
+        <AnimatedText />
+     </div>
     </div>
 
   )
