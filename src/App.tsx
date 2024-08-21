e80ef9e34d8b0f08a5be52f6e7fe56b7cef2f82c
twani <jbestius@gmail.com>
2024-08-21 21:04:21 +0200

    feat: add gsap animation to all the titles

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,13 @@ import styles from './App.module.css';
 function App() {
 
   return (
-    <div className={`flex flex-col items-center justify-center h-screen w-screen bg-black relative ${styles.main}`}>
+    <div className={`flex flex-col items-center justify-center h-screen w-screen bg-black relative ${styles.main} overflow-hidden`}>
       <div className={`${styles.myGradient} absolute z-[1] h-full w-full`}></div>
       <video autoPlay muted loop className='h-full w-full object-cover'>
         <source src="/videos/myVideo.webm" type="video/webm"/>
         Your browser does not support the video tag.
       </video>
-     <div className="absolute z-[2]">
+     <div className="absolute z-[2] h-full w-full flex items-center justify-center">
         <AnimatedText />
      </div>
     </div>
--- src/components/animatedText.tsx
+++ src/components/animatedText.tsx
@@ -1,9 +1,48 @@
-import React from 'react'
+
+import { useEffect} from 'react'
+import gsap from "gsap"
+import { TextPlugin } from "gsap/TextPlugin";
+import SplitType from 'split-type'
+
+
+gsap.registerPlugin(TextPlugin);
+
 
 const AnimatedText = () => {
+    
+useEffect(() => {
+    const titles = document.querySelectorAll('.myText');
+        titles.forEach((title) => {
+            (title as HTMLElement).style.opacity = '1';
+        });
+        const myTitles = gsap.utils.toArray(".myText");
+        const tl = gsap.timeline({repeat: -1, repeatDelay: 1});
+
+        if (myTitles) {
+            myTitles.forEach((title:any) => {
+                
+                const split = new SplitType(title);
+                tl.to(split.chars, {
+                    duration: 2,
+                    opacity: 1,
+                    stagger: 0.12,
+                    visibility: 'visible',
+                    animationDuration: 0.5,
+                    repeat: 1,
+                    yoyo: true,
+                });
+            })
+        }
+
+},[])
+
+
   return (
-    <div className="py-8">
-        <h1 className="text-[4rem] text-slate-100 font-bold abhaya-libre-bold">Hello people</h1>
+    <div  className="py-8 relative h-full w-full flex justify-center items-center">
+        <h1  className="myText text-[4rem] text-slate-100 font-bold abhaya-libre-bold ">Hello people</h1>
+        <h1 className="myText text-[4rem] text-slate-100 font-bold abhaya-libre-bold ">Hello Humans</h1>
+        <h1 className="myText text-[4rem] text-slate-100 font-bold abhaya-libre-bold ">Hello Laura</h1>
+        <h1 className="myText text-[4rem] text-slate-100 font-bold abhaya-libre-bold ">Hello Uganda</h1>
     </div>
   )
 }
